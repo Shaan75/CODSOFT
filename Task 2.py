a3e36946f31e74b2ac5b40a00d900000f725473f
Shaan75 <141444469+Shaan75@users.noreply.github.com>
2024-06-21 21:48:59 +0530

    Task 2.py

--- Task 2.py
+++ Task 2.py
@@ -1,18 +1,22 @@
 print("~~~~~~~~~~~Mini Calculator~~~~~~~~~~~")
-n1= float(input("Enter number :"))
-n2= float(input("Enter another number :"))
-print("1. Addition\n")
-print("2. Substraction\n")
-print("3. Multiplication\n")
-print("4. Divvision\n")
-choice = int(input("your choice from 1-4: "))
-if choice==1:
-    print("Addition= ",n1+n2)
-elif choice==2:
-    print("Substraction= ",n1-n2)
-elif choice==3:
-    print("Multiplication= ",n1*n2)
-elif choice==4:
-    print("Division= ",n1/n2)
-else:
-    print("INVALID INPUT")
+while True:
+    n1= float(input("Enter number :"))
+    n2= float(input("Enter another number :"))
+    print("1. Addition\n")
+    print("2. Substraction\n")
+    print("3. Multiplication\n")
+    print("4. Divvision\n")
+    print("5. Exit\n")
+    choice = int(input("your choice from 1-5: "))
+    if choice==1:
+        print("Addition= ",n1+n2)
+    elif choice==2:
+        print("Substraction= ",n1-n2)
+    elif choice==3:
+        print("Multiplication= ",n1*n2)
+    elif choice==4:
+        print("Division= ",n1/n2)
+    elif choice==5:
+        break
+    else:
+        print("INVALID INPUT")
